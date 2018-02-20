ecfc3bb702e4e059acf73c2ce562331f830be64d
pazdernikpavel <33874597+pazdernikpavel@users.noreply.github.com>
2018-02-20 19:11:22 +0100

    Extending photo uploader

--- DatingApp.SPA/src/app/members/photo-editor/photo-editor.component.ts
+++ DatingApp.SPA/src/app/members/photo-editor/photo-editor.component.ts
@@ -1,7 +1,10 @@
+import { User } from './../../_models/User';
 import { Component, OnInit } from '@angular/core';
 import { Input } from '@angular/core';
 import { Photo } from '../../_models/Photo';
 import { FileUploader } from 'ng2-file-upload';
+import { environment } from '../../../environments/environment';
+import { AuthService } from '../../_services/auth.service';
 
 @Component({
   selector: 'app-photo-editor',
@@ -10,16 +13,44 @@ import { FileUploader } from 'ng2-file-upload';
 })
 export class PhotoEditorComponent implements OnInit {
   @Input() photos: Photo[];
-  uploader: FileUploader = new FileUploader({});
+  uploader: FileUploader;
   hasBaseDropZone = false;
+  baseUrl = environment.apiUrl;
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.initializeUploader();
   }
 
   public fileOverBase(e: any): void {
     this.hasBaseDropZone = e;
+  }
+
+  initializeUploader() {
+    this.uploader = new FileUploader({
+      url: this.baseUrl + 'users/' + this.authService.decodedToken.nameid + '/photos',
+      authToken: 'Bearer ' + localStorage.getItem('token'),
+      isHTML5: true,
+      allowedFileType: ['image'],
+      removeAfterUpload: true,
+      autoUpload: false,
+      maxFileSize: 10 * 1024 * 1024
+    });
+
+    this.uploader.onSuccessItem = (item, response, status, headers) => {
+      if (response) {
+        const res: Photo = JSON.parse(response);
+        const photo = {
+          id: res.id,
+          url: res.url,
+          dateAdded: res.dateAdded,
+          description: res.description,
+          isMain: res.isMain
+        };
+        this.photos.push(photo);
+      }
+    }
   }
 
 }
